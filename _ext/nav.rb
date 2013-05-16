0cd7066a9afe77f870af0d46b034aedfee9397bf
Pete Muir <pmuir@bleepbleep.org.uk>
2013-05-16 14:58:44 +0100

    Fix navigation tab activation

---
+++ _ext/nav.rb
@@ -0,0 +1,45 @@
+module JBoss
+  module Windup
+    module Awestruct
+      module Extensions
+        class Nav
+          def initialize()
+          end
+
+          def execute(site)
+            if site.nav
+              nav = {}
+              site.nav.each do |k, v|
+                nav[k.to_s] = build(v, '', k, site)
+              end
+              site.nav = nav
+            end
+          end
+
+          def build(object, href, parent, site)
+            if parent != nil
+              href = "#{href}/#{parent}.html"
+            end
+            r = {} 
+            if Hash === object
+              object.each do |key, value|
+                if key.to_s == 'label' || key.to_s == 'href' || key.to_s == 'description'
+                  r[key] = value
+                end
+              end
+            end
+            s = OpenStruct.new(r)
+            puts s
+            s.href ||= href
+            if s.href !~ /^http[s]?:\/\//
+              s.href = site.base_url + s.href
+            end 
+            s.label ||= parent.to_s.split('-').each{|word| word.capitalize!}.join(' ')
+            s
+          end
+        end
+      end
+    end
+  end
+end
+
--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,5 +1,6 @@
 require File.join File.dirname(__FILE__), 'documentation'
 require File.join File.dirname(__FILE__), 'report'
+require File.join File.dirname(__FILE__), 'nav'
 require 'wget_wrapper'
 require 'js_minifier'
 require 'css_minifier'
@@ -10,6 +11,7 @@ require 'compass_config'
 Awestruct::Extensions::Pipeline.new do
   extension JBoss::Windup::Awestruct::Extensions::Documentation.new('/windup.wiki', '.adoc')
   extension JBoss::Windup::Awestruct::Extensions::Report.new('sample')
+  extension JBoss::Windup::Awestruct::Extensions::Nav.new
   helper Awestruct::Extensions::Partial
   extension Awestruct::Extensions::WgetWrapper.new
   transformer Awestruct::Extensions::JsMinifier.new
